da6c6923deff2373c2684f30e46d06bb28eaf3da
yangfengdi <yangfengdi@gmail.com>
2022-02-18 14:43:07 +0800

    更新词汇记忆与测验数据。

--- word_utils.py
+++ word_utils.py
@@ -95,15 +95,15 @@ if __name__ == '__main__':
     utils = db_utils()
 
     #导入单词记忆事件
-    #utils.import_remember_events('words/remember')
+    utils.import_remember_events('words/remember')
 
     #导入单词测验结果
-    #utils.import_quiz_result('words/quiz')
+    utils.import_quiz_result('words/quiz')
 
     #制作测试题
     #word_set.make_quiz_from_fail_record('20210520', 30)
     #word_set.make_quiz_from_remembered_some_days_words('20220209', 30)
-    word_set.make_quiz_from_tag('俞敏洪初中', '2022-02-18', 8)
+    #word_set.make_quiz_from_tag('俞敏洪初中', '2022-02-18', 8)
 
     #创建新词列表
     #word_set.get_new_words_from_articles('words/article')
